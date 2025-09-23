27a0ca2d7ef34fad2f47f24ca4e2e635270f98c0
chetan7528 <kaklotarchetan7528@gmail.com>
2025-09-23 11:05:31 +0530

    minor changes

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createContent, getContent } = require('@controllers/contentController');
 const auth = require('@middlewares/authMiddleware');
 
-router.post('/createContent', auth('user', 'provider', 'admin'), createContent);
-router.get('/getContent', auth('user', 'provider', 'admin'), getContent);
+router.post('/createContent', auth('admin'), createContent);
+router.get('/getContent', getContent);
 
 module.exports = router;
